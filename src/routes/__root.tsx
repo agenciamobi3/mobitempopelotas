e6e15c47765e58720fec884e376b07cea6e3abcb
agenciamobi <157939955+agenciamobi@users.noreply.github.com>
2026-08-30 03:46:01 -0300

    feat(adsense): adicionar script global do Google AdSense

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -37,6 +37,7 @@ import appCss from "../styles.css?url";
 const GOOGLE_ANALYTICS_MEASUREMENT_ID = "G-97YX7HPD90";
 const GOOGLE_ANALYTICS_IDLE_TIMEOUT_MS = 3_000;
 const GOOGLE_ANALYTICS_FALLBACK_DELAY_MS = 1_500;
+const GOOGLE_ADSENSE_CLIENT_ID = "ca-pub-4545997973925216";
 const PUBLIC_RUNTIME_RELEASE = "2026-08-29-ana-rhn-contract-v2";
 
 type AnalyticsWindow = Window & {
@@ -239,6 +240,11 @@ gtag('config', '${GOOGLE_ANALYTICS_MEASUREMENT_ID}', {
     <html lang="pt-BR">
       <head>
         <HeadContent />
+        <script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${GOOGLE_ADSENSE_CLIENT_ID}`}
+          crossOrigin="anonymous"
+        />
         <script dangerouslySetInnerHTML={{ __html: analyticsBootstrap }} />
       </head>
       <body>
