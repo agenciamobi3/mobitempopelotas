ddcbe7478151d0256c3f32b95c37bdfa22400a50
MOBI - Marketing Inteligente <157939955+agenciamobi@users.noreply.github.com>
2026-09-09 22:40:38 -0300

    refactor(hydrology): move Sao Goncalo to editorial shell

--- src/routes/nivel-do-canal-sao-goncalo.tsx
+++ src/routes/nivel-do-canal-sao-goncalo.tsx
@@ -5,7 +5,8 @@ import {
   DefesaCivilStationHydrologyPage,
   type DefesaCivilStationPageConfig,
 } from "@/components/hydrology/DefesaCivilStationHydrologyPage";
-import { ContentPageShell } from "@/components/layout/ContentPageShell";
+import "@/components/hydrology/DefesaCivilStationVisualRefresh.css";
+import "@/components/hydrology/HydrologyEditorialRoute.css";
 import { getDefesaCivilHydroData } from "@/lib/hydrology/defesa-civil-rs.functions";
 import { createPageHead } from "@/lib/page-meta";
 import { createEditorialPageJsonLd, createFaqPageJsonLd } from "@/lib/structured-data";
@@ -131,9 +132,9 @@ function NivelCanalSaoGoncaloPage() {
   const data = Route.useLoaderData();
 
   return (
-    <ContentPageShell pageClassName="content-shell--defesa-civil-station">
+    <div className="hydrology-editorial-route hydrology-editorial-route--defesa-civil-station hydrology-editorial-route--sao-goncalo">
       <DefesaCivilStationHydrologyPage data={data} config={PAGE_CONFIG} />
       <EditorialContentSection id="como-interpretar-nivel-canal-sao-goncalo" content={PAGE_CONTENT} />
-    </ContentPageShell>
+    </div>
   );
 }
